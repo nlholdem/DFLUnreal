18293b3f00800b921e417a7f94df256fe616f141
Paul Miller <nlholdem@hotmail.com>
2017-12-14 14:24:10 +0000

    Adding code to render to a rendertarget texture; set the vehicles to red so we can detect when they appear in view.

--- Source/CarAIProjectCPP/DFLWheeledVehicle.cpp
+++ Source/CarAIProjectCPP/DFLWheeledVehicle.cpp
@@ -12,12 +12,13 @@ ADFLWheeledVehicle::ADFLWheeledVehicle()
 	PrimaryActorTick.bCanEverTick = true;
 	RootComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Root"));
 	Camera = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Camera"));
-//	ConstructorHelpers::FObjectFinder<UTextureRenderTarget2D> RenderTargetAsset(TEXT("/Game/MyRenderTarget"));
-	//here you need to have prepared MyLittleRenderTarget asset, type RenderTarget2D. You can have one for many actors, it is duplicated. What is not resolved by me: i don't know if it is stable solution or it will make crash after many calls
+	ConstructorHelpers::FObjectFinder<UTextureRenderTarget2D> RenderTargetAsset(TEXT("/Game/Textures/CameraInput"));
+	//here you need to have prepared MyLittleRenderTarget asset, type RenderTarget2D. You can have one for many actors, 
+	//it is duplicated. What is not resolved by me: i don't know if it is stable solution or it will make crash after many calls
 
-//	RenderTarget = DuplicateObject(RenderTargetAsset.Object, NULL);
-//	RenderTarget->InitAutoFormat(1024, 1024);
-//	Camera->TextureTarget = RenderTarget;
+	RenderTarget = DuplicateObject(RenderTargetAsset.Object, NULL);
+	RenderTarget->InitAutoFormat(1024, 1024);
+	Camera->TextureTarget = RenderTarget;
 
 	UE_LOG(LogTemp, Warning, TEXT("Constructing DFL_Vehicle!!"));
 
@@ -27,6 +28,43 @@ ADFLWheeledVehicle::ADFLWheeledVehicle()
 void ADFLWheeledVehicle::BeginPlay()
 {
 	Super::BeginPlay();
+
+	Camera->TextureTarget = RenderTarget;
+
+	int X = RenderTarget->GetSurfaceHeight();
+	int Y = RenderTarget->GetSurfaceWidth();
+	GLog->Logf(TEXT("Size: %d %d"), X, Y);
+	Texture2D = RenderTarget->ConstructTexture2D(this, FString("Tex2D"), EObjectFlags::RF_NoFlags);
+	//FTextureRenderTargetResource *Resource = RenderTarget->GetRenderTargetResource();
+	int xx = Texture2D->GetSizeX();
+	int yy = Texture2D->GetSizeY();
+	GLog->Logf(TEXT("Texture size: %d %d"), xx, yy);
+
+	FTexturePlatformData *Data = Texture2D->PlatformData;
+
+	EPixelFormat Format = Data->PixelFormat;
+	GLog->Logf(TEXT("Pixel format: %d"), (uint8)(Format));
+	//format of pixel is PFloatRGBA
+
+	int size = Data->Mips[0].BulkData.GetElementSize();
+	int N = Data->Mips[0].BulkData.GetElementCount();
+	GLog->Logf(TEXT("Number of elements: %d, size of one element: %d"), N, size);
+	//i've got 1 byte size of element
+
+	const void *Table = Data->Mips[0].BulkData.LockReadOnly();
+	Data->Mips[0].BulkData.Unlock();
+
+	const uint16 *Tab2 = StaticCast<const uint16*>(Table);
+	//ok, quick calculation. I get 8*1024*1024 bytes from 1024*1024 pixels, so one pixel got 8 bytes of data. Format is RGBA, so you can figure it yourself :)
+	for (int i = 0; i<xx; i++)
+		for (int j = 0; j < yy; j++) {
+			int k = 4 * (i*yy + j);
+			int R = Tab2[k];
+			int G = Tab2[k + 1];
+			int B = Tab2[k + 2];
+			int A = Tab2[k + 3];
+		}
+
 
 }
 
